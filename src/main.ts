27d5a3be10ff95ded0c4bd5da4f30a17af75c957
LucasLvy <lucaslevy10@gmail.com>
2022-06-08 23:50:08 +0200

    feat(Contract): offline address computation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { readFileSync } from 'fs';
-import { defaultProvider, ec, stark } from 'starknet';
-import { getSelectorFromName } from 'starknet/dist/utils/hash';
+import { AddTransactionResponse, defaultProvider, ec, stark } from 'starknet';
+import { getSelectorFromName, computeHashOnElements } from 'starknet/dist/utils/hash';
 
-async function deployAcc(compiledContract: string, calldata: string[], salt: string) {
+// Argent X account contract class hash (needed for proxy deployment)
+const ACCOUNT_CLASS_HASH = '0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328';
+
+// Argent X proxy contract class hash (needed for address computation)
+const PROXY_CLASS_HASH =
+  '1072035497076846607322037267784110659410341675694031857644447227002241788184';
+
+const COMPILED_PROXY = readFileSync('./compiled_contracts/Proxy.json', 'utf-8');
+
+/**
+ * @param  {string} compiledContract - Compiled contract file content
+ * @param  {string[]} calldata - constructor arguments
+ * @param  {string} salt - salt used for the deployment
+ * @returns Promise - { code: string, transaction_hash: string, address: string }
+ */
+async function deployAcc(
+  compiledContract: string,
+  calldata: string[],
+  salt: string
+): Promise<AddTransactionResponse> {
   const deployTransaction = await defaultProvider.deployContract({
     contract: compiledContract,
     constructorCalldata: calldata,
@@ -11,17 +30,33 @@ async function deployAcc(compiledContract: string, calldata: string[], salt: str
   return deployTransaction;
 }
 
-// Argent X account contract class hash (needed for proxy deployment)
-const ACCOUNT_CLASS_HASH = '0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328';
+/**
+ * @param  {string} contractClassHash - class hash of the contract
+ * @param  {string} salt - salt used for the deployment
+ * @param  {string[]} constructorCalldata - constructor arguments
+ * @returns Promise - contract address as hex string
+ */
+async function computeAddress(
+  contractClassHash: string,
+  salt: string,
+  constructorCalldata: string[]
+): Promise<string> {
+  const PREFIX = '523065374597054866729014270389667305596563390979550329787219';
+  // the ASCII encoding of the constant string “STARKNET_CONTRACT_ADDRESS”
+  const CALLER_ADDRESS = 0; //  currently always zero
 
-// Argent X proxy contract class hash (needed for address computation)
-const PROXY_CLASS_HASH =
-  '692771186250089447853187296611718889956438341042390535747407094000177116738';
-
-const COMPILED_PROXY = readFileSync('./Proxy.json', 'utf-8');
+  const address = computeHashOnElements([
+    PREFIX,
+    CALLER_ADDRESS,
+    salt,
+    contractClassHash,
+    computeHashOnElements(constructorCalldata),
+  ]);
+  return address;
+}
 
 async function main() {
-  // Generate a random key pair
+  // Generating a random key pair
   const starkPair = ec.genKeyPair();
   const starkPub = ec.getStarkKey(starkPair);
 
@@ -41,6 +76,12 @@ async function main() {
     calldata: contractInitializationCalldata,
   });
 
-  console.log(await deployAcc(COMPILED_PROXY, constructorCalldata, salt));
+  // Precomputing the proxy contract address
+  const expectedAddress = await computeAddress(PROXY_CLASS_HASH, salt, constructorCalldata);
+
+  // Checking that the expected address is indeed the actual address
+  if (expectedAddress !== (await deployAcc(COMPILED_PROXY, constructorCalldata, salt)).address) {
+    throw new Error("Computed address doesn't match actual address");
+  }
 }
 main();
