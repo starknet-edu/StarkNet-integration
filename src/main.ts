d7e3cb9728fa4f6ddc754c7d3ec5e3824009e46a
LucasLvy <lucaslevy10@gmail.com>
2022-06-24 14:25:18 +0200

    feat(script): main only deploys account

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { readFileSync } from 'fs';
-import { AddTransactionResponse, defaultProvider, ec, stark } from 'starknet';
-import { getSelectorFromName, computeHashOnElements } from 'starknet/dist/utils/hash';
+import { defaultProvider, ec, stark, number, hash } from 'starknet';
+
+const { toFelt } = number;
+const { getSelectorFromName, computeHashOnElements } = hash;
 
 // Argent X account contract class hash (needed for proxy deployment)
 const ACCOUNT_CLASS_HASH = '0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328';
@@ -10,25 +12,6 @@ const PROXY_CLASS_HASH =
   '1072035497076846607322037267784110659410341675694031857644447227002241788184';
 
 const COMPILED_PROXY = readFileSync('./compiled_contracts/Proxy.json', 'utf-8');
-
-/**
- * @param  {string} compiledContract - Compiled contract file content
- * @param  {string[]} calldata - constructor arguments
- * @param  {string} salt - salt used for the deployment
- * @returns Promise - { code: string, transaction_hash: string, address: string }
- */
-async function deployAcc(
-  compiledContract: string,
-  calldata: string[],
-  salt: string
-): Promise<AddTransactionResponse> {
-  const deployTransaction = await defaultProvider.deployContract({
-    contract: compiledContract,
-    constructorCalldata: calldata,
-    addressSalt: salt,
-  });
-  return deployTransaction;
-}
 
 /**
  * @param  {string} contractClassHash - class hash of the contract
@@ -43,6 +26,7 @@ async function computeAddress(
 ): Promise<string> {
   const PREFIX = '523065374597054866729014270389667305596563390979550329787219';
   // the ASCII encoding of the constant string “STARKNET_CONTRACT_ADDRESS”
+
   const CALLER_ADDRESS = 0; //  currently always zero
 
   const address = computeHashOnElements([
@@ -79,9 +63,20 @@ async function main() {
   // Precomputing the proxy contract address
   const expectedAddress = await computeAddress(PROXY_CLASS_HASH, salt, constructorCalldata);
 
+  // Deploying the account contract
+  const deploymentTx = await defaultProvider.deployContract({
+    contract: COMPILED_PROXY,
+    constructorCalldata: constructorCalldata,
+    addressSalt: salt,
+  });
+
+  deploymentTx.address = deploymentTx.address ?? '';
+
   // Checking that the expected address is indeed the actual address
-  if (expectedAddress !== (await deployAcc(COMPILED_PROXY, constructorCalldata, salt)).address) {
+  if (toFelt(expectedAddress) !== toFelt(deploymentTx.address)) {
     throw new Error("Computed address doesn't match actual address");
   }
+  await defaultProvider.waitForTransaction(deploymentTx.transaction_hash);
 }
+
 main();
